1418b1008922ac9e6ef3dafc239ff2c4005aa2c6
Lodi Van Dijk <49106018+lodivandijk@users.noreply.github.com>
2024-05-25 21:23:02 +0100

    Add files via upload

---
+++ stockProcessor/src/test/java/com/example/stockProcessor/StockProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
